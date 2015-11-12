b58ab2d0b42bd48e8ae4c2011fff27314ab149f0
jamesagnew <jamesagnew@gmail.com>
2015-11-12 07:28:25 -0500

    Initial commit of example project

---
+++ java/hapi-fhir-skeleton-client/src/main/java/test/TestApplication.java
@@ -0,0 +1,16 @@
+package test;
+
+import ca.uhn.fhir.context.FhirContext;
+
+public class TestApplication {
+
+	/**
+	 * This is the Java main method, which gets executed
+	 */
+	public static void main(String[] args) {
+
+		FhirContext ctx = FhirContext.forDstu2();
+		
+	}
+
+}

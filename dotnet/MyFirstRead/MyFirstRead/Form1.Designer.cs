e4fcb2a2dc78fc6d00a1b6a237f6a9660bc11309
Mirjam Baltus <m.baltus@furore.com>
2015-11-17 20:12:52 +0100

    Added first Read project

---
+++ dotnet/MyFirstRead/MyFirstRead/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MyFirstRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(65, 140);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(148, 48);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtError
+            // 
+            this.txtError.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtError.ForeColor = System.Drawing.Color.Red;
+            this.txtError.Location = new System.Drawing.Point(12, 194);
+            this.txtError.Multiline = true;
+            this.txtError.Name = "txtError";
+            this.txtError.ReadOnly = true;
+            this.txtError.Size = new System.Drawing.Size(258, 55);
+            this.txtError.TabIndex = 1;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(258, 122);
+            this.txtDisplay.TabIndex = 2;
+            this.txtDisplay.Text = "Click Start to retrieve data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtError;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+

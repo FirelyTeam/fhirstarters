52c38af3501f8f7bf8b74d89af5012750fabf7ec
James Agnew <jamesagnew@gmail.com>
2015-11-17 18:14:20 +0100

    Update example

--- java/hapi-fhir-skeleton-client/src/main/java/test/TestApplication.java
+++ java/hapi-fhir-skeleton-client/src/main/java/test/TestApplication.java
@@ -1,6 +1,8 @@
 package test;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.model.dstu2.resource.Patient;
+import ca.uhn.fhir.rest.client.IGenericClient;
 
 public class TestApplication {
 
@@ -9,8 +11,19 @@ public class TestApplication {
 	 */
 	public static void main(String[] args) {
 
+		// Create a context
 		FhirContext ctx = FhirContext.forDstu2();
-		
+
+		// Create a client
+		IGenericClient client = ctx.newRestfulGenericClient("http://fhirtest.uhn.ca/baseDstu2");
+
+		// Read a patient with the given ID
+		Patient patient = client.read().resource(Patient.class).withId("952975").execute();
+
+		// Print the output
+		String string = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(patient);
+		System.out.println(string);
+
 	}
 
 }

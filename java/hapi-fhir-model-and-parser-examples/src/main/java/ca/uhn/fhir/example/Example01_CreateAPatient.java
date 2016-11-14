bd1e8a9f312d8bc35780eb3039fd510e7617e3a7
James Agnew <jamesagnew@gmail.com>
2016-11-13 19:53:37 -0500

    Examples

---
+++ java/hapi-fhir-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example01_CreateAPatient.java
@@ -0,0 +1,27 @@
+package ca.uhn.fhir.example;
+
+import org.hl7.fhir.dstu3.model.HumanName;
+import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.Patient;
+
+public class Example01_CreateAPatient {
+
+	public static void main(String[] theArgs) {
+		
+		// Create a resource instance
+		Patient pat = new Patient();
+		
+		// Add a "name" element
+		HumanName name = pat.addName();
+		name.addFamily("Simpson").addGiven("Homer").addGiven("J");
+		
+		// Add an "identifier" element
+		Identifier identifier = pat.addIdentifier();
+		identifier.setSystem("http://acme.org/MRNs").setValue("7000135");
+
+		// Model is designed to be chained
+		pat.addIdentifier().setSystem("http://acme.org/MRNs").setValue("12345");
+		
+	}
+	
+}

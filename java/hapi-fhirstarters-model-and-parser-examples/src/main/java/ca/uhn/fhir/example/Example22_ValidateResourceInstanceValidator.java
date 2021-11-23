f4fb0ae46024747cb17f9e342fe485de354a0c8f
jamesagnew <jamesagnew@gmail.com>
2021-11-23 09:59:01 -0500

    Dependency bumps

--- java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example22_ValidateResourceInstanceValidator.java
+++ java/hapi-fhirstarters-model-and-parser-examples/src/main/java/ca/uhn/fhir/example/Example22_ValidateResourceInstanceValidator.java
@@ -1,11 +1,11 @@
 package ca.uhn.fhir.example;
 
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.validation.FhirValidator;
-import org.hl7.fhir.dstu3.hapi.ctx.DefaultProfileValidationSupport;
-import org.hl7.fhir.dstu3.hapi.validation.FhirInstanceValidator;
-import org.hl7.fhir.dstu3.model.Encounter;
+import org.hl7.fhir.common.hapi.validation.validator.FhirInstanceValidator;
+import org.hl7.fhir.r4.model.Encounter;
 
 public class Example22_ValidateResourceInstanceValidator {
    public static void main(String[] args) {
@@ -14,17 +14,18 @@ public class Example22_ValidateResourceInstanceValidator {
       enc.addIdentifier().setSystem("http://acme.org/encNums").setValue("12345");
 
       // Create a new validator
-      FhirValidator validator = FhirContext.forDstu3().newValidator();
+      FhirContext fhirContext = FhirContext.forR4();
+      FhirValidator validator = fhirContext.newValidator();
 
       // Cache this object! Supplies structure definitions
-      DefaultProfileValidationSupport support = new DefaultProfileValidationSupport();
+      DefaultProfileValidationSupport support = new DefaultProfileValidationSupport(fhirContext);
 
       // Create the validator
       FhirInstanceValidator module = new FhirInstanceValidator(support);
       validator.registerValidatorModule(module);
 
       // Did we succeed?
-      IParser parser = FhirContext.forDstu3().newXmlParser().setPrettyPrint(true);
+      IParser parser = FhirContext.forR4().newXmlParser().setPrettyPrint(true);
       System.out.println(parser.encodeResourceToString(validator.validateWithResult(enc).toOperationOutcome()));
    }
 }
